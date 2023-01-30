c9fa6265b5474790ed555fcbaeae8b8d3dbc331c
Charlie Cheater <charliecheaters@yandex.ru>
2023-01-30 09:54:25 +0300

    Added interface

---
+++ SofaScoreApi.Client/Service/TeamService/ITeamContext.cs
@@ -0,0 +1,9 @@
+﻿using SofaScoreApi.DAL.Models;
+
+namespace SofaScoreApi.Client.Service.TeamService;
+
+public interface ITeamContext
+{
+    Task<IEnumerable<SportEvent>> GetLastEvents(int teamId);
+    Task<IEnumerable<SportEvent>> GetNextEvents(int teamId);
+}
